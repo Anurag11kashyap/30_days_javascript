ed29bc9219a0c1b0e393a060b71e105dc5b9aeb6
Anurag kashyap <anurag.kashyap4861@gmail.com>
2024-07-18 16:57:20 +0530

    modify day_6 activity 5

--- Day_6.js
+++ Day_6.js
@@ -56,9 +56,11 @@ console.log(element)
 });
 
 //Activity 5-
-let arr02=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14]]
+let arr02=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]
 for(let i=0;i<arr02.length;i++){
-console.log(arr02[i]);}
+    for(let j=0 ;j<arr02[0].length;j++)
+        console.log(arr02[i][j]);
+    }
 
 
 //2-
